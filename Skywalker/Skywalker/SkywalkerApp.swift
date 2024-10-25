190562249bf19a15122a9e83badd845c1ebae5d8
AxLentz <acocoanuts@gmail.com>
2024-10-25 22:16:41 +0800

    create project

---
+++ Skywalker/Skywalker/SkywalkerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkywalkerApp.swift
+//  Skywalker
+//
+//  Created by Jason on 25/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct SkywalkerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Skywalker/SkywalkerTests/SkywalkerTests.swift
@@ -0,0 +1,17 @@
+//
+//  SkywalkerTests.swift
+//  SkywalkerTests
+//
+//  Created by Jason on 25/10/2024.
+//
+
+import Testing
+@testable import Skywalker
+
+struct SkywalkerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
